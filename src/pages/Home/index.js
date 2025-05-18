3d44dfc80b3908bce32e9ac0b731771c9ab22547
MohammadMuneebAhmed <muneebahmed2786@gmail.com>
2025-05-18 19:19:43 +0530

    initial_setup_td1

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
