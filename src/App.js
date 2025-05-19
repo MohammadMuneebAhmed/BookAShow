49822a438ea5323768f60ebcd11d014d4045dea4
MohammadMuneebAhmed <muneebahmed2786@gmail.com>
2025-05-19 20:14:49 +0530

    first commit td_1 and td_2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import "./stylesheets/alignments.css";
+import "./stylesheets/custom.css";
+import "./stylesheets/form-elements.css";
+import "./stylesheets/sizes.css";
+import "./stylesheets/theme.css";
 function App() {
   return (
     <div>
